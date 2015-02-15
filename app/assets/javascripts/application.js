6fc2a21884a6323b1fefc1d010ae87c4de41daea
dirtyRuby <Dirty-Glamer@yandex.ru>
2015-02-15 23:18:06 +0200

    Fix javascripts.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,20 +15,19 @@
 //= require turbolinks
 //= require_tree .
 
-function slider (owningClass, submissiveClass){
-    $(owningClass).click(function(){
-        // If block invisible then make visible with slide down.
-        if ($(submissiveClass).css('display') == "none" ) {
-            $(submissiveClass).slideDown(500);
-        }
-        // If block visible then make invisible with slide up.
-        else {
-            $(submissiveClass).slideUp(500);
-        }
-    });
-}
-
 $(document).ready(function(){
+    function slider (owningClass, submissiveClass){
+        $(owningClass).click(function(){
+            // If block invisible then make visible with slide down.
+            if ($(submissiveClass).css('display') == "none" ) {
+                $(submissiveClass).slideDown(500);
+            }
+            // If block visible then make invisible with slide up.
+            else {
+                $(submissiveClass).slideUp(500);
+            }
+        });
+    }
 
     // Function for top menu buttons.
 
