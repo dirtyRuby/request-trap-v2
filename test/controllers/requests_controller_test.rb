3defe6c55bc038fc214fb17a58a103f4340ec6d8
dirtyRuby <Dirty-Glamer@yandex.ru>
2015-02-21 03:40:28 +0200

    Functional tests for requests.

--- test/controllers/requests_controller_test.rb
+++ test/controllers/requests_controller_test.rb
@@ -8,8 +8,16 @@ class RequestsControllerTest < ActionController::TestCase
   end
 
   test "should get show" do
-    get :show, id: @my_request.id, trap_name: @my_trap.name
+    get :show, id: @my_request, trap_name: @my_trap.name
     assert_response :success
+  end
+
+  test "should destroy request" do
+    assert_difference('Request.count', -1) do
+      delete :destroy, id:@my_request, trap_name: @my_trap.name
+    end
+
+    assert_redirected_to trap_path(trap_name: @my_trap.name)
   end
 
 end
